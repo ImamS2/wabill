679c6cf6a84fbaa23205379498b532710b247d02
afarhansib <afarhansib@gmail.com>
2022-12-07 06:02:19 +0700

    changing baileys to fork of baileys, some ui/ux improvement

---
+++ baileys.js
@@ -0,0 +1,100 @@
+"use strict"
+
+const {
+	default:makeWASocket,
+  DisconnectReason,
+	useMultiFileAuthState,
+	delay
+} = require("baileys")
+const pino = require('pino')
+const { rm } = require("fs");
+
+const sendMessageWTyping = async(sock, msg, jid) => {
+	await sock.presenceSubscribe(jid)
+	await delay(500)
+
+	await sock.sendPresenceUpdate('composing', jid)
+	await delay(2000)
+
+	await sock.sendPresenceUpdate('paused', jid)
+
+	await sock.sendMessage(jid, msg)
+}
+
+const baileys = async (wa, updateQR) => {
+  const {state, saveCreds} = await useMultiFileAuthState('session')
+
+  const sock = makeWASocket({
+    printQRInTerminal: true,
+    browser: ["Wabill", "Chrome", "1.1.0"],
+    logger: pino({
+      level: 'error'
+    }),
+    auth: state
+  })
+
+  const sendMessageWTyping = async(msg, jid) => {
+		await sock.presenceSubscribe(jid)
+		await delay(500)
+
+		await sock.sendPresenceUpdate('composing', jid)
+		await delay(2000)
+
+		await sock.sendPresenceUpdate('paused', jid)
+
+		await sock.sendMessage(jid, msg)
+	}
+
+  sock.ev.process(
+		async(events) => {
+			if(events['connection.update']) {
+				const update = events['connection.update']
+				const { connection, lastDisconnect } = update
+				if(connection === 'close') {
+					if(lastDisconnect?.error?.output?.statusCode !== DisconnectReason.loggedOut) {
+						baileys(wa, updateQR)
+					} else {
+            console.log('Connection closed. You are logged out.')
+            rm("./session", { recursive: true }, (err) => {
+              if (err && err.code == "ENOENT") {
+                // file doens't exist
+                console.info("Folder doesn't exist, won't remove it.");
+              } else if (err) {
+                console.error("Error occurred while trying to remove folder.");
+                console.error(err)
+              }
+            });
+						baileys(wa, updateQR)
+					}
+				}
+
+				// console.log('connection update', update)
+        // console.log(JSON.stringify(update))
+        updateQR(update)
+			}
+
+			if(events['creds.update']) {
+				await saveCreds()
+			}
+
+			// if(events['messages.upsert']) {
+			// 	const upsert = events['messages.upsert']
+			// 	// console.log('recv messages ', JSON.stringify(upsert, undefined, 2))
+
+			// 	if(upsert.type === 'notify') {
+			// 		for(const msg of upsert.messages) {
+			// 			if(!msg.key.fromMe && msg.key.remoteJid === "6281233745324@s.whatsapp.net") {
+			// 				console.log('replying to', msg.key.remoteJid)
+			// 				await sock.readMessages([msg.key])
+			// 				await sendMessageWTyping(sock, { text: 'Hello there!' }, msg.key.remoteJid)
+			// 			}
+			// 		}
+			// 	}
+			// }
+    }
+  )
+
+	return sock
+}
+
+module.exports = { baileys, sendMessageWTyping }
--- index.js
+++ index.js
@@ -1,193 +1,114 @@
-const http = require("http");
-const express = require("express");
-const qrcode = require("qrcode");
-const socketIO = require("socket.io");
-const { unlink } = require("fs");
-// baileys
-const makeWASocket = require("@adiwajshing/baileys").default;
-const {
-  DisconnectReason,
-  makeWALegacySocket,
-} = require("@adiwajshing/baileys");
+const http = require("http")
+const express = require("express")
+const qrcode = require("qrcode")
+const socketIO = require("socket.io")
 
-const port = 8000 || process.env.PORT;
-const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
+const { baileys, sendMessageWTyping } = require("./baileys")
+const { json } = require("express")
 
-app.use(express.json());
-// app.use(express.urlencoded({
-//   extended: true
-// }))
-app.use("/assets", express.static(__dirname + "/client/assets"));
+const port = 8000 || process.env.PORT
+const app = express()
+const server = http.createServer(app)
+const io = socketIO(server)
 
-app.get("/scan", (req, res) => {
-  res.sendFile("./client/server.html", {
-    root: __dirname,
-  });
-});
+app.use(express.json())
+app.use("/assets", express.static(__dirname + "/client/assets"))
 
 app.get("/", (req, res) => {
   res.sendFile("./client/index.html", {
     root: __dirname,
-  });
-});
+  })
+})
 
-let config = {
-  printQRInTerminal: true,
-  browser: ["Wabill", "Chrome", "1.1.0"],
-};
+let wa
+let sock
+let qr
+let connected
 
-let wa;
-let sock;
-let qr;
-
-const startWA = async (currentVersion = "legacy") => {
-  if (currentVersion === "legacy") {
-    wa = makeWALegacySocket(config);
-  } else {
-    unlink("./auth-info-multi.json", (err) => {
-      if (err && err.code == "ENOENT") {
-        // file doens't exist
-        console.info("File doesn't exist, won't remove it.");
-      } else if (err) {
-        console.error("Error occurred while trying to remove file.");
-      }
-    });
-    wa = makeWASocket(config);
+const updateQR = (data) => {
+  const setQR = qr => {
+    qrcode.toDataURL(qr, (err, url) => {
+      sock?.emit("qr", url)
+      sock?.emit("log", "QR Code received, please scan!")
+    })
   }
 
-  wa.ev.on("connection.update", (update) => {
-    let statusCode = update.lastDisconnect?.error?.output?.statusCode;
-    let msg = update.lastDisconnect?.error?.output?.payload?.message;
+  // console.log('qr: ' + data.qr)
+  // console.log('connection: ' + data.connection)
+  if (data.qr) {
+    qr = data.qr
+    setQR(qr)
+  }
 
-    console.log(update);
-    // error with legacy version
-    if (
-      update.lastDisconnect?.error?.toString() ===
-      "Error: Unexpected server response: 400"
-    ) {
-      // console.log("Error, please restart the app!")
-      setTimeout(() => {
-        startWA();
-      }, 1024);
-    }
+  if (data === 'qr') {
+    setQR(qr)
+  }
 
-    switch (statusCode) {
-      case DisconnectReason.restartRequired:
-        config.auth = wa.authState;
-        startWA("md");
-        updateQR("connected");
-        break;
-      case DisconnectReason.timedOut:
-        console.log("QR Timeout. Trying again...");
-        startWA("md");
-        break;
-      case DisconnectReason.multideviceMismatch:
-        console.log("Hmm. Trying again...");
-        if (msg === "Require multi-device edition") {
-          startWA("md");
-          break;
-        }
-        startWA();
-        break;
-      default:
-        break;
-    }
+  if (data.connection === 'open' || data === 'connected') {
+    connected = true
+    sock?.emit("qrstatus", "./assets/check.svg")
+    sock?.emit("log", "WhatsApp terhubung!")
+  }
+  
+  if (data.connection === 'close') {
+    connected = false
+    sock?.emit("qrstatus", "./assets/loader.gif")
+  }
+}
 
-    if (update.qr) {
-      qr = update.qr;
-      updateQR("qr");
-    } else {
-      qr = undefined;
-      if (update.connection === "open") {
-        updateQR("connected");
-        return;
-      }
-      updateQR("loading");
-    }
-  });
-};
-
-startWA();
+(async() => {
+  wa = await baileys(wa, updateQR)
+})()
 
 io.on("connection", async (socket) => {
-  sock = socket;
+  sock = socket
 
-  // console.log(wa)
-  if (isConnected) {
-    updateQR("connected");
-  } else if (qr) updateQR("qr");
-});
+  // console.log(await wa.onWhatsApp())
+  if (connected) {
+    updateQR("connected")
+  } else if (qr) updateQR('qr')
+})
 
 // send text message
 app.post("/send-message", async (req, res) => {
   // console.log(JSON.stringify(req.headers))
   // console.log(req)
-  const message = req.body.message;
-  const number = req.body.number;
+  const message = req.body.message
+  const number = req.body.number
 
+  // console.log(wa.onWhatsApp())
+  // return
   // console.log(await wa.onWhatsApp(number))
-  if (isConnected) {
-    const exists = await wa.onWhatsApp(number);
+  if (connected) {
+    const exists = await wa.onWhatsApp(number)
     if (exists?.jid || (exists && exists[0]?.jid)) {
-      wa.sendMessage(exists.jid || exists[0].jid, { text: message })
+      sendMessageWTyping(wa, { text: message }, exists.jid || exists[0].jid)
         .then((result) => {
           res.status(200).json({
             status: true,
             response: result,
-          });
+          })
         })
         .catch((err) => {
           res.status(500).json({
             status: false,
             response: err,
-          });
-        });
+          })
+        })
     } else {
       res.status(500).json({
         status: false,
         response: `Nomor ${number} tidak terdaftar.`,
-      });
+      })
     }
   } else {
     res.status(500).json({
       status: false,
       response: `WhatsApp belum terhubung.`,
-    });
+    })
   }
-});
-
-// functions
-const updateQR = (data = "qr") => {
-  switch (data) {
-    case "qr":
-      qrcode.toDataURL(qr, (err, url) => {
-        sock?.emit("qr", url);
-        sock?.emit("log", "QR Code received, please scan!");
-      });
-      break;
-    case "connected":
-      sock?.emit("qrstatus", "./assets/check.svg");
-      sock?.emit("log", "WhatsApp terhubung!");
-      break;
-    case "loading":
-      sock?.emit("qrstatus", "./assets/loader.gif");
-      sock?.emit("log", "QR Code received, please scan!");
-      break;
-    default:
-      break;
-  }
-};
-
-const isConnected = () => {
-  return (
-    (wa.type === "md" && wa.user) ||
-    (wa.type === "legacy" && wa.state?.legacy?.phoneConnected)
-  );
-};
+})
 
 server.listen(port, () => {
-  console.log(`Aplikasi berjalan di http://localhost:${port}`);
-  console.log(`Scan QR di http://localhost:${port}/scan`);
-});
+  console.log(`Aplikasi berjalan di http://localhost:${port}`)
+})
